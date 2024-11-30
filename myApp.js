53f244e1155041b6f45717c2c83d156896fbe2a9
Tunde-Fadex Oluwatumininu <FA7@FADEX-PC.(none)>
2024-11-30 18:48:08 +0100

    Add initial application

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`FA7 Info Security App Started on Port ${PORT}`);
+});
 
 
 
